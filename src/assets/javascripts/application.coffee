380b406098a09c9cabe1178b791e4a87e8cea5c1
yukung <yukung.i@gmail.com>
2015-09-28 21:33:14 +0900

    Add asset pipeline plugins coffeescript and sass.

---
+++ src/assets/javascripts/application.coffee
@@ -0,0 +1,7 @@
+console.log "Loading Custom Javascript..."
+
+timer = () ->
+    snippet = "Timer Triggered"
+    console.log snippet
+
+setInterval timer, 1000
--- src/assets/javascripts/application.js
+++
@@ -1,8 +0,0 @@
-console.log("Loading Custom Javascript...");
-
-var timer = function() {
-    var snippet = "Timer Triggered";
-    console.log(snippet);
-};
-
-setInterval(timer, 1000);
