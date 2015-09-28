08cf7578456149dce332f7cf871a37166f6bfcf7
yukung <yukung.i@gmail.com>
2015-09-28 17:41:25 +0900

    Revert "Remove unnecessary files."
    This reverts commit 3117b5a0a8e400c1ce23e8d5af085d8d118b1f11.

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,12 +1,20 @@
 import asset.pipeline.ratpack.AssetPipelineModule
+import ratpack.groovy.template.MarkupTemplateModule
 
+import static ratpack.groovy.Groovy.groovyMarkupTemplate
 import static ratpack.groovy.Groovy.ratpack
 
 ratpack {
     bindings {
+        module MarkupTemplateModule
         module AssetPipelineModule
     }
 
     handlers {
+        get {
+            render groovyMarkupTemplate("index.gtpl", title: "My Boilerplate")
+        }
+
+        files { dir "public" }
     }
 }
---
+++ src/ratpack/templates/index.gtpl
@@ -0,0 +1,10 @@
+yieldUnescaped '<!DOCTYPE html>'
+html {
+  head {
+    title "Site boilerplate $title"
+    link href: '/css/application.css', rel: 'stylesheet'
+  }
+  body {
+    h1 'Boilerplate'
+  }
+}
